2f1ce84f2e827fa913a0468f810fe55e90960971
gbosdet <gbosdet@gmail.com>
2019-06-12 06:43:43 -0700

    Functional

--- Cartpole.py
+++ Cartpole.py
@@ -2,11 +2,11 @@ import gym
 import numpy as np
 
 env = gym.make("CartPole-v0")
-observation = env.reset()
+
 
 box = env.observation_space
 
-done = False
+
 
 action = 0
 weights = np.zeros(4)
@@ -17,19 +17,34 @@ for trial in range(1000):
     current_weights = np.random.randn(4)
     for episode in range(40):
         count = 0
+        done = False
+        observation = env.reset()
         while not done:
-            if observation.dot(weights) >= 0:
+            if observation.dot(current_weights) >= 0:
                 action = 1
             else:
                 action = 0
             observation, reward, done, info = env.step(action)
             count += 1
         sum += count
+
     if sum > weight_sum:
         weight_sum = sum
         weights = current_weights
     if trial % 50 == 0:
-        print("Weights: " + str(weights))
-        print("Sum: " + str(weight_sum / 40))
-print("Weights: " + str(weights))
-print("Sum: " + str(weight_sum / 40))
+        print()
+        print("Trial: " + str(trial))
+        print("\tWeights: " + str(weights))
+        print("\tSum: " + str(weight_sum))
+print("Final Information")
+print("\tWeights: " + str(weights))
+print("\tSum: " + str(weight_sum / 40))
+done = False
+observation = env.reset()
+while not done:
+    if observation.dot(weights) >= 0:
+        action = 1
+    else:
+        action = 0
+    observation, reward, done, info = env.step(action)
+    env.render()
