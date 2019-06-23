4848c06443e18712132c050b93e7ddf07c0be466
gbosdet <gbosdet@gmail.com>
2019-06-22 22:48:56 -0700

    Data Collecting Near Final

--- LunarLander2.py
+++ LunarLander2.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import pickle
 
 class DDQN:
-    def __init__(self, num_inputs, num_outputs, hidden_layer_sizes=[80, 80], activation_function="relu", optimizer="adam", loss_function="mean_squared_error", gamma=0.99, experience_buffer_size=80000, min_buffer_size=50, learning_rate=0.0005, training_batch_size=50, training_times_per_weight_copy=10):
+    def __init__(self, num_inputs, num_outputs, hidden_layer_sizes=[80, 80], activation_function="relu", optimizer="adam", loss_function="mean_squared_error", gamma=0.99, experience_buffer_size=80000, min_buffer_size=50, learning_rate=0.001, training_batch_size=64, training_times_per_weight_copy=50):
         self.num_inputs = num_inputs
         self.num_outputs = num_outputs
         self.gamma = gamma
@@ -63,7 +63,7 @@ class DDQN:
             for i in range(self.training_batch_size):
                 experience = self.experiences[indexes[i]]
                 y[i] = state_predicted_values[i]
-                y[i][experience[1]] = experience[2] if experience[4] else experience[2] + np.max(next_state_predicted_values[i])
+                y[i][experience[1]] = experience[2] if experience[4] else experience[2] + self.gamma*np.max(next_state_predicted_values[i])
             self.model.fit(X, y, batch_size=self.training_batch_size, verbose=0)
             self.training_iterations += 1
             if self.training_iterations % self.training_times_per_weight_copy == 0:
@@ -83,13 +83,13 @@ class DDQN:
         for i in range(len(self.experiences)):
             experience = self.experiences[indexes[i]]
             y[i] = state_predicted_values[i]
-            y[i][experience[1]] = experience[2] if experience[4] else experience[2] + np.max(next_state_predicted_values[i])
+            y[i][experience[1]] = experience[2] if experience[4] else experience[2] + self.gamma*y[i-1][self.experiences[indexes[i-1]][1]]
         self.model.fit(X, y, batch_size=len(self.experiences), verbose=0)
         self.training_iterations += 1
         if self.training_iterations % 10 == 0:
             self.target_model.set_weights(self.model.get_weights())
 
-def run_episode(env, ddqn, random_action_rate):
+def run_episode(env, ddqn, random_action_rate, show_video):
     last_state = env.reset()
     done = False
     reward_sum = 0
@@ -97,6 +97,8 @@ def run_episode(env, ddqn, random_action_rate):
     while not done and iterations < 3000:
         action = ddqn.get_action(last_state, random_action_rate)
         next_state, reward, done, info = env.step(action)
+        if show_video:
+            env.render()
        # next_state = next_state.reshape(1, len(next_state))
         ddqn.add_experience((last_state, action, reward, next_state, done))
         last_state = next_state
@@ -112,12 +114,12 @@ def reward_mod(reward, last_state, current_state):
 
 def learn_game():
     sns.set()
-    random_action_rate = 0.4
-    random_action_decay_rate = 0.002
+    random_action_rate = 0.6
+    random_action_decay_rate = 0.99
 
     env = gym.make('LunarLander-v2')
     # ddqn = DDQN(len(env.observation_space.sample()), env.action_space.n)
-    ddqn = DDQN(8, env.action_space.n, hidden_layer_sizes=[64], gamma=0.999, training_times_per_weight_copy=50)
+    ddqn = DDQN(8, env.action_space.n, hidden_layer_sizes=[64, 64], gamma=0.99, training_times_per_weight_copy=50)
     #ddqn.add_experience(((0, 0, 0, 0, 0, 0, 1, 1), 0, 1000, (0, 0, 0, 0, 0, 0, 1, 1), True))
     # num_found = 0
     # while num_found < 200:
@@ -141,33 +143,58 @@ def learn_game():
     good_experiences = []
     with open("good_episodes.txt", "rb") as file:
         ddqn.experiences = pickle.load(file)
-        good_experiences = ddqn.experiences
+        good_experiences = deque([x for x in ddqn.experiences])
     for i in range(200):
         ddqn.initial_train()
     rewards = []
     steps_to_finish = []
     for i in range(3000):
-        random_action_rate = 1/np.sqrt(i+1)
-        reward, steps = run_episode(env, ddqn, random_action_rate)
+        #random_action_rate = 1/np.sqrt(i+1)
+        reward, steps = run_episode(env, ddqn, random_action_rate, (i%20==0))
         rewards.append(reward)
         steps_to_finish.append(steps)
-        #random_action_rate -= random_action_decay_rate
+        random_action_rate *= random_action_decay_rate
         if i % 20 == 0:
             print("Episode:", i, "\tReward:", reward, "\tSteps:", steps)
-        if i == 600 or i == 1200:
-            temp = ddqn.experiences
-            ddqn.experiences = good_experiences
-            for j in range(200):
-                ddqn.initial_train()
-            ddqn.experiences = temp
+        if i % 500 == 0 :
+            # temp = ddqn.experiences
+            # ddqn.experiences = good_experiences
+            # for j in range(200):
+            #     ddqn.initial_train()
+            # ddqn.experiences = temp
+            # for experience in good_experiences:
+            #     ddqn.add_experience(experience)
+            plt.plot(rewards)
+            plt.title("Reward per Episode")
+            plt.ylabel("Episode Reward")
+            plt.xlabel("Episode")
+            plt.savefig(("Reward vs Episode " + str(i)))
+            plt.tight_layout()
+            plt.close()
+            plt.plot(steps_to_finish)
+            plt.title("Steps per Episode")
+            plt.ylabel("Episode Reward")
+            plt.xlabel("Episode")
+            plt.tight_layout()
+            plt.savefig(("Steps vs Episode " + str(i)))
+            plt.close()
+
 
 
     plt.plot(rewards)
-    plt.title("Rewards")
-    plt.show()
+    plt.title("Reward per Episode")
+    plt.ylabel("Episode Reward")
+    plt.xlabel("Episode")
+    plt.savefig(("Reward vs Episode"))
+    plt.tight_layout()
+    plt.close()
     plt.plot(steps_to_finish)
-    plt.title("Steps")
-    plt.show()
+    plt.title("Steps per Episode")
+    plt.ylabel("Episode Reward")
+    plt.xlabel("Episode")
+    plt.tight_layout()
+    plt.savefig("Steps vs Episode")
+    plt.close()
 
 if __name__ == "__main__":
     learn_game()
